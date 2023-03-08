ddcfd9df0b7e05319842626cae18e4788d8b4bf2
arafat-rimon <rimonarafat2020@gmail.com>
2023-03-08 22:45:07 +0600

    first commit

---
+++ src/test/java/com/example/hospital_managemet_system/HospitalManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital_managemet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
